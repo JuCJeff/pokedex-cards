137dbcaeaf05c9d3b301f5ea7b2074186bb84d5e
Jeff Ma <jucjeff96@gmail.com>
2024-10-23 20:50:13 -0500

    Fix: Render trophy group only when count is greater than 0

--- src/components/TrophyList.jsx
+++ src/components/TrophyList.jsx
@@ -3,16 +3,16 @@ import "./TrophyList.css";
 import PropTypes from "prop-types";
 import { GiTrophyCup } from "react-icons/gi";
 
-const TrophyList = ({ count, color }) => (
-  <div className="trophy-group">
-    {count > 0 &&
-      [...Array(count)].map((_, index) => (
+const TrophyList = ({ count, color }) =>
+  count > 0 ? (
+    <div className="trophy-group">
+      {[...Array(count)].map((_, index) => (
         <li key={`${color}-${index}`}>
           <GiTrophyCup color={color} className="trophy-cup" />
         </li>
       ))}
-  </div>
-);
+    </div>
+  ) : null;
 
 TrophyList.propTypes = {
   count: PropTypes.number.isRequired,
